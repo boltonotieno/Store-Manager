13c65a5d428be98a54f8a99cf220acb78bcc1240
Bolton <bolton.otieno@gmail.com>
2018-11-13 13:32:38 +0300

    [Chore #161877092] add admin login test

---
+++ test/main.js
@@ -0,0 +1,42 @@
+
+var storage = require('mock-local-storage')
+var request = require('request');
+var should = require('should');
+var chai = require('chai');
+var baseURL = "https://my-store-manager-api.herokuapp.com/api/v2"
+var assert = chai.assert;
+
+describe('Array', function() {
+  it('should start empty', function() {
+    var arr = [];
+
+    assert.equal(arr.length, 0);
+  });
+});
+
+describe('User Login', function() {
+  it('should login a user', function(done) {
+      let data = {
+      username: 'admin',
+      password: 'adminpass'
+    }
+    request.post({
+      url: baseURL + '/auth/login',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data) 
+     }, 
+      function(error, response, body){
+        var statusCode = response.statusCode;
+        var bodyObj = JSON.parse(body)
+        localStorage.setItem('access_token', bodyObj.access_token);
+        assert.equal(statusCode, 200)
+        assert.equal(bodyObj.message, "Logged in succesful as admin")
+        done();
+      }
+    );
+  });
+});
+
+
