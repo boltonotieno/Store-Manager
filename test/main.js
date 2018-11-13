9292ddb696a2e9e89c7b4550e816b33df3ac8da8
Bolton <bolton.otieno@gmail.com>
2018-11-13 15:16:35 +0300

    [Chore #161877092] add timeout

--- test/main.js
+++ test/main.js
@@ -4,6 +4,7 @@ var request = require('request');
 var should = require('should');
 var chai = require('chai');
 var baseURL = "https://my-store-manager-api.herokuapp.com/api/v2"
+
 var assert = chai.assert;
 
 // Test Function
@@ -15,133 +16,136 @@ describe('Array', function() {
   });
 });
 
-// Test User Login
-describe('User Login', function() {
-  it('should login a user', function(done) {
-      let data = {
-      username: 'admin',
-      password: 'adminpass'
-    }
-    request.post({
-      url: baseURL + '/auth/login',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify(data) 
-     }, 
-      function(error, response, body){
-        var statusCode = response.statusCode;
-        var bodyObj = JSON.parse(body)
-        localStorage.setItem('access_token', bodyObj.access_token);
-        assert.equal(statusCode, 200)
-        assert.equal(bodyObj.message, "Logged in succesful as admin")
-        done();
-      }
-    );
-  });
-});
+describe('Admin Tests', function() {
+  this.timeout(5000)
+    // Test User Login
+    describe('User Login', function() {
+      it('should login a user', function(done) {
+          let data = {
+          username: 'admin',
+          password: 'adminpass'
+        }
+        request.post({
+          url: baseURL + '/auth/login',
+          headers: {
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify(data) 
+        }, 
+          function(error, response, body){
+            var statusCode = response.statusCode;
+            var bodyObj = JSON.parse(body)
+            localStorage.setItem('access_token', bodyObj.access_token);
+            assert.equal(statusCode, 200)
+            assert.equal(bodyObj.message, "Logged in succesful as admin")
+            done();
+          }
+        );
+      });
+    });
 
-// Test GET all Users
-describe('Get all Users', function() {
-  it('should get all users', function(done) {
-    const token = localStorage.getItem('access_token')
-    request.get({
-      url:baseURL + '/users',
-      headers: {
-        "Authorization": "Bearer " + token
-      }
-    }, 
-      function(error, response, body){
-        var statusCode = response.statusCode;
-        var bodyObj = JSON.parse(body)
-        assert.equal(statusCode, 200);
-        assert.equal(bodyObj.message, "Users successfully retrieved")
-        done();
-      }
-    );
-  });
-});
+    // Test GET all Users
+    describe('Get all Users', function() {
+      it('should get all users', function(done) {
+        const token = localStorage.getItem('access_token')
+        request.get({
+          url:baseURL + '/users',
+          headers: {
+            "Authorization": "Bearer " + token
+          }
+        }, 
+          function(error, response, body){
+            var statusCode = response.statusCode;
+            var bodyObj = JSON.parse(body)
+            assert.equal(statusCode, 200);
+            assert.equal(bodyObj.message, "Users successfully retrieved")
+            done();
+          }
+        );
+      });
+    });
 
-// Test GET one user by id
-describe('Get one User', function() {
-  it('should get one user', function(done) {
-    const token = localStorage.getItem('access_token')
-    request.get({
-      url:baseURL + '/users/1',
-      headers: {
-        "Authorization": "Bearer " + token
-      }
-    }, 
-      function(error, response, body){
-        var statusCode = response.statusCode;
-        var bodyObj = JSON.parse(body)
-        assert.equal(statusCode, 200);
-        assert.equal(bodyObj.message, "User successfully retrieved")
-        done();
-      }
-    );
-  });
-});
+    // Test GET one user by id
+    describe('Get one User', function() {
+      it('should get one user', function(done) {
+        const token = localStorage.getItem('access_token')
+        request.get({
+          url:baseURL + '/users/1',
+          headers: {
+            "Authorization": "Bearer " + token
+          }
+        }, 
+          function(error, response, body){
+            var statusCode = response.statusCode;
+            var bodyObj = JSON.parse(body)
+            assert.equal(statusCode, 200);
+            assert.equal(bodyObj.message, "User successfully retrieved")
+            done();
+          }
+        );
+      });
+    });
 
-// Test GET all Categories
-describe('Get Categories', function() {
-  it('should get all cetegories', function(done) {
-    const token = localStorage.getItem('access_token')
-    request.get({
-      url:baseURL + '/category',
-      headers: {
-        "Authorization": "Bearer " + token
-      }
-    }, 
-      function(error, response, body){
-        var statusCode = response.statusCode;
-        var bodyObj = JSON.parse(body)
-        assert.equal(statusCode, 200);
-        assert.equal(bodyObj.message, "Categories successfully retrieved")
-        done();
-      }
-    );
-  });
-});
+    // Test GET all Categories
+    describe('Get Categories', function() {
+      it('should get all cetegories', function(done) {
+        const token = localStorage.getItem('access_token')
+        request.get({
+          url:baseURL + '/category',
+          headers: {
+            "Authorization": "Bearer " + token
+          }
+        }, 
+          function(error, response, body){
+            var statusCode = response.statusCode;
+            var bodyObj = JSON.parse(body)
+            assert.equal(statusCode, 200);
+            assert.equal(bodyObj.message, "Categories successfully retrieved")
+            done();
+          }
+        );
+      });
+    });
 
-// Test GET all Products
-describe('Get Products', function() {
-  it('should get all products', function(done) {
-    const token = localStorage.getItem('access_token')
-    request.get({
-      url:baseURL + '/products',
-      headers: {
-        "Authorization": "Bearer " + token
-      }
-    }, 
-      function(error, response, body){
-        var statusCode = response.statusCode;
-        var bodyObj = JSON.parse(body)
-        assert.equal(statusCode, 200);
-        assert.equal(bodyObj.message, "Products successfully retrieved")
-        done();
-      }
-    );
-  });
-});
+    // Test GET all Products
+    describe('Get Products', function() {
+      it('should get all products', function(done) {
+        const token = localStorage.getItem('access_token')
+        request.get({
+          url:baseURL + '/products',
+          headers: {
+            "Authorization": "Bearer " + token
+          }
+        }, 
+          function(error, response, body){
+            var statusCode = response.statusCode;
+            var bodyObj = JSON.parse(body)
+            assert.equal(statusCode, 200);
+            assert.equal(bodyObj.message, "Products successfully retrieved")
+            done();
+          }
+        );
+      });
+    });
 
-// Test GET all Sales
-describe('Get Sales', function() {
-  it('should get all sales', function(done) {
-    const token = localStorage.getItem('access_token')
-    request.get({
-      url:baseURL + '/sales',
-      headers: {
-        "Authorization": "Bearer " + token
-      }
-    }, 
-      function(error, response, body){
-        var statusCode = response.statusCode;
-        var bodyObj = JSON.parse(body)
-        assert.equal(statusCode, 200);
-        assert.equal(bodyObj.message, "Sales successfully retrieved")
-        done();
-      }
-    );
-  });
+    // Test GET all Sales
+    describe('Get Sales', function() {
+      it('should get all sales', function(done) {
+        const token = localStorage.getItem('access_token')
+        request.get({
+          url:baseURL + '/sales',
+          headers: {
+            "Authorization": "Bearer " + token
+          }
+        }, 
+          function(error, response, body){
+            var statusCode = response.statusCode;
+            var bodyObj = JSON.parse(body)
+            assert.equal(statusCode, 200);
+            assert.equal(bodyObj.message, "Sales successfully retrieved")
+            done();
+          }
+        );
+      });
+    });
 });
